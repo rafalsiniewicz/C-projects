c9cf8ca42a186c028e48b8b2231023a8e348d635
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2018-10-21 01:55:44 +0200

    Update main.c

--- Calendar/main.c
+++ Calendar/main.c
@@ -1,1 +1,12 @@
+#include "calendar.h"
+int main()
+{
+    printf("Calendar application shows dates from January 2000 to December 2049\n");
+    printf("Click Enter to display calendar\n");
+    getchar();
+    system("cls");
+    menu(0,2018);
+    return 0;
+}
+
 
